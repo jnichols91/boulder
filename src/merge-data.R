1df86f3bb0626caa94eade3c514561af027a7834
Justin Nichols <niko@Justins-MacBook-Pro-2.local>
2020-06-09 19:22:09 -0600

    Finish merging the four data sets into one

--- src/merge-data.R
+++ src/merge-data.R
@@ -20,6 +20,28 @@ phosfax_hourly <- phosfax_10m %>%
 phosfax_hourly$date <- with(phosfax_hourly, ymd_h(paste(date, hour, sep= ' ')))
 phosfax_hourly <- phosfax_hourly %>% select(date, op_conc_mg_p_l_hourly)
 
+# similar as above get into the same format. just include the hour of the day
+mixed_liqour_hourly <- mixed_liqour_hourly %>% 
+  mutate(hour = hour(date)) %>% 
+  mutate(date = date(date))
+mixed_liqour_hourly$date <- with(mixed_liqour_hourly, ymd_h(paste(date, hour, sep= ' ')))
+mixed_liqour_hourly <- mixed_liqour_hourly %>% select(1:2)
+
+# only grab the day and coagulant to be merged later 
+dosing_daily_coag <- dosing_daily[,1:2]
+
+temp <- inner_join(mixed_liqour_hourly, phosfax_hourly, by = "date")
+temp2 <- inner_join(temp, flow_hourly, by = "date")
+
+# add the coagulant so reseparate the hour and date since dosing is only daily and not hourly... will restructure after
+temp2 <- temp2 %>% 
+  mutate(hour = hour(date), date = date(date))
+final_data <- inner_join(temp2, dosing_daily_coag, by = "date")
+
+# now restructure
+final_data$date <- with(final_data, ymd_h(paste(date, hour, sep= ' ')))
+# bring the coagulant to the second column 
+final_data <- final_data %>% select(date, coagulant, everything())
 
 
 
