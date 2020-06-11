251bdd85e43daf19418d70bb72e44f59a736d95d
Justin Nichols <niko@Justins-MacBook-Pro-2.local>
2020-06-10 23:10:03 -0600

    Cleaned up files

--- src/eda-final.R
+++ src/eda-final.R
@@ -15,12 +15,6 @@ load("../data/boulderMoWater.rda")
 
 table(final_data$coagulant)
 
-# splitting the data by coagulant 
-
-ferr_data <- final_data %>% filter(coagulant == "Ferric")
-alum_data <- final_data %>% filter(coagulant == "Alum")
-none_data <- final_data %>% filter(coagulant == "None")
-
 # average was found by hour maybe do a moving average instead?  
 
 # time series plots 
@@ -41,7 +35,7 @@ alumphos <- phosfax_10m %>% # phosfax only alum coagulant every 10 minutes
 
 rects1 <- data.frame(xstart = as.POSIXct('2019-11-06 15:00:00'), 
                     xend = as.POSIXct('2019-11-13 00:00:00'))
-
+# thursday plot 1
 alumphos %>% ggplot() + 
   geom_point(aes(date, op_conc_mg_p_l), size=1.25) + 
   labs(title = "Alum Effluent OP 10-minute Intervals",
@@ -80,18 +74,6 @@ ferrphos %>% ggplot() +
   geom_rect(data = rects2, aes(xmin = xstart, xmax = xend, 
                               ymin = -0.15, ymax = Inf),fill = "red",col = "red", alpha = 0.1) + 
   theme(plot.title = element_text(face="bold"))
-# numerous values of 0.0004; is this the default error value---sensor fault
-
-# variables that have NA's...dealing with them
-
-final_data$influent_mgd_hourly_avg[4:18]
-final_data$daft_sub_gpm[6:17]
-final_data$daft_sub_gal[8:21]
-final_data$abi_mgd[1:12]
-final_data$ras_gpm[1:12]
-final_data$mlws_flow_gpm[7:18]
-
-# remove? fill with average, moving average? 
 
 ferr_data %>% ggplot() + 
   geom_point(aes(date, op_mg_p_l), col = "blue") + 
@@ -105,63 +87,104 @@ alum_data %>% ggplot() +
 final_data %>% ggplot() + 
   geom_point(aes(op_mg_p_l, op_conc_mg_p_l_hourly))
 
+ferr_data <- ferr_data %>% mutate(hour = hour(date))
+alum_data <- alum_data %>% mutate(hour = hour(date))
 
-alum_melt1 <- melt(alum_data[,c(3:4)], measure.vars=colnames(alum_data[,c(3:4)]))
-alum_melt2 <- melt(alum_data[,c(5:6,10,19:20,22)], measure.vars=colnames(alum_data[,c(5:6,10,19:20,22)]))
-alum_melt3 <- melt(alum_data[,c(7:8,12)], measure.vars=colnames(alum_data[,c(7:8,12)]))
-alum_melt4 <- melt(alum_data[,c(14:15)], measure.vars=colnames(alum_data[,c(14:15)]))
-alum_melt5 <- melt(alum_data[,c(18)])
-alum_melt6 <- melt(alum_data[,c(21)])
-alum_melt7 <- melt(alum_data[,c(9,11,13)], measure.vars=colnames(alum_data[,c(9,11,13)]))
-alum_melt8 <- melt(alum_data[,c(16:17)], measure.vars=colnames(alum_data[,c(16:17)]))
+alum_melt1 <- melt(alum_data[,c(8,15)], id.vars = "hour")
+alum_melt2 <- melt(alum_data[,c(13,15)], id.vars = "hour")
+alum_melt3 <- melt(alum_data[,c(9:10,15)], id.vars = "hour")
+alum_melt4 <- melt(alum_data[,c(6,11,15)], id.vars = "hour")
+alum_melt5 <- melt(alum_data[,c(3:4,15)], id.vars = "hour")
+alum_melt6 <- melt(alum_data[,c(5,7,12,14,15)], id.vars = "hour")
 
-ferr_melt1 <- melt(ferr_data[,c(3:4)], measure.vars=colnames(ferr_data[,c(3:4)]))
-ferr_melt2 <- melt(ferr_data[,c(5:6,10,19:20,22)], measure.vars=colnames(ferr_data[,c(5:6,10,19:20,22)]))
-ferr_melt3 <- melt(ferr_data[,c(7:8,12)], measure.vars=colnames(ferr_data[,c(7:8,12)]))
-ferr_melt4 <- melt(ferr_data[,c(14:15)], measure.vars=colnames(ferr_data[,c(14:15)]))
-ferr_melt5 <- melt(ferr_data[,c(18)])
-ferr_melt6 <- melt(ferr_data[,c(21)])
-ferr_melt7 <- melt(ferr_data[,c(9,11,13)], measure.vars=colnames(ferr_data[,c(9,11,13)]))
-ferr_melt8 <- melt(ferr_data[,c(16:17)], measure.vars=colnames(ferr_data[,c(16:17)]))
+ferr_melt1 <- melt(alum_data[,c(8,15)], id.vars = "hour")
+ferr_melt2 <- melt(alum_data[,c(13,15)], id.vars = "hour")
+ferr_melt3 <- melt(alum_data[,c(9:10,15)], id.vars = "hour")
+ferr_melt4 <- melt(alum_data[,c(6,11,15)], id.vars = "hour")
+ferr_melt5 <- melt(alum_data[,c(3:4,15)], id.vars = "hour")
+ferr_melt6 <- melt(alum_data[,c(5,7,12,14,15)], id.vars = "hour")
 
 ggplot(alum_melt1) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(alum_melt2) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(alum_melt3) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(alum_melt4) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(alum_melt5) +
-  geom_boxplot(aes(x="mlws_flow_gpm",y=value, fill="mlws_flow_gpm"))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(alum_melt6) +
-  geom_boxplot(aes(x = "twas_flow_gal", y=value, fill="twas_flow_gal"))
-ggplot(alum_melt7) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
-ggplot(alum_melt8) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 
 ggplot(ferr_melt1) +
-  geom_boxplot(aes(x=variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(ferr_melt2) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(ferr_melt3) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(ferr_melt4) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(ferr_melt5) +
-  geom_boxplot(aes(x="mlws_flow_gpm",y=value, fill="mlws_flow_gpm"))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 ggplot(ferr_melt6) +
-  geom_boxplot(aes(x = "twas_flow_gal", y=value, fill="twas_flow_gal"))
-ggplot(ferr_melt7) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
-ggplot(ferr_melt8) +
-  geom_boxplot(aes(x = variable, y=value, fill=variable))
+  geom_boxplot(aes(y=value, fill=variable), outlier.size = 0.5, outlier.color = "red") + 
+  facet_wrap(vars(hour)) + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
 
-t.test(alum_data$op_conc_mg_p_l_hourly, ferr_data$op_conc_mg_p_l_hourly)
+# the percent change in op levels 
 
-ferr_diff <- ferr_data$op_mg_p_l - ferr_data$op_conc_mg_p_l_hourly
-alum_diff <- alum_data$op_mg_p_l - alum_data$op_conc_mg_p_l_hourly
+ferr_diff_pct <- (ferr_data$op_mg_p_l - ferr_data$op_conc_mg_p_l_hourly)/ferr_data$op_mg_p_l * 100
+alum_diff_pct <- (alum_data$op_mg_p_l - alum_data$op_conc_mg_p_l_hourly)/alum_data$op_mg_p_l * 100
+
+# shows that alum can be concluded as better
+t.test(alum_diff_pct, ferr_diff_pct)
 
 
 
--- src/merge-data.R
+++ src/merge-data.R
@@ -45,8 +45,65 @@ final_data <- inner_join(temp2, dosing_daily_coag, by = "date")
 final_data$date <- with(final_data, ymd_h(paste(date, hour, sep= ' ')))
 # bring the coagulant to the second column 
 final_data <- final_data %>% select(date, coagulant, everything()) %>% select(-hour)
+final_data$centrate_gal[100:101] <- 0
 
-save(final_data, file = "../data/final-data.rda")
+# morning talk thursday
+# sensor issues refer to plot of red boxed ferric area 
+
+final_data <- final_data %>% 
+  filter(date(date) != ymd("2019-08-30") & date(date) != ymd("2019-09-02")) # plot insert number ferric issue
+
+final_data <- final_data %>% 
+  filter(date(date) < ymd("2019-11-13") | date(date) > ymd("2019-12-01")) # plot alum issue
+
+# vars Kate recommended to remove 
+del_vars <- c("influent_mgd_daily_avg", "primary_sludge_gmp_daily_avg", "primary_sludge_gal", "thickened_sludge_gal",
+              "gvt_o_f_gpm", "daft_sub_gpm", "daft_sub_gal", "twas_flow_gpm_daily_avg") 
+
+final_data <- final_data %>% 
+  select(-all_of(del_vars)) 
+
+# look at NA's and mention approach 
+
+for (i in 8:12) {
+  temp <- final_data %>% filter(hour(date) == i & is.na(influent_mgd_hourly_avg))
+  value <- final_data %>% 
+    filter(hour(date) == i) %>%  
+    select(influent_mgd_hourly_avg) %>% 
+    summarise(mean(influent_mgd_hourly_avg, na.rm = TRUE))
+  final_data$influent_mgd_hourly_avg[final_data$date == temp$date] <- as.numeric(value)
+  
+  temp <- final_data %>% filter(hour(date) == i & is.na(mlws_flow_gpm))
+  value <- final_data %>% 
+    filter(hour(date) == i) %>%  
+    select(mlws_flow_gpm) %>% 
+    summarise(mean(mlws_flow_gpm, na.rm = TRUE))
+  final_data$mlws_flow_gpm[final_data$date == temp$date] <- as.numeric(value)
+}
+
+for (i in 7:13) {
+  temp <- final_data %>% filter(hour(date) == i & is.na(abi_mgd))
+  value <- final_data %>% 
+    filter(hour(date) == i) %>%  
+    select(abi_mgd) %>% 
+    summarise(mean(abi_mgd, na.rm = TRUE))
+  final_data$abi_mgd[final_data$date == temp$date] <- as.numeric(value)
+  
+  temp <- final_data %>% filter(hour(date) == i & is.na(ras_gpm))
+  value <- final_data %>% 
+    filter(hour(date) == i) %>%  
+    select(ras_gpm) %>% 
+    summarise(mean(ras_gpm, na.rm = TRUE))
+  final_data$ras_gpm[final_data$date == temp$date] <- as.numeric(value)
+}
+
+final_data <- final_data %>% 
+  filter(mlws_flow_gpm > 100) # SCADA system down 
+
+ferr_data <- final_data %>% filter(coagulant == "Ferric")
+alum_data <- final_data %>% filter(coagulant == "Alum")
+
+# save(final_data, ferr_data, alum_data, file = "../data/final-data.rda")
 
 
 
